26151b38207df96a827a9177f1ed6c7828d35b80
ian <i@u1t.dev>
2026-05-20 11:10:53 -0400

    do primary function

---
+++ modules/GenerateCompletion.ts
@@ -0,0 +1,33 @@
+/** @format */
+
+import * as DevKit from "./typing.ts";
+
+export default function GenerateCompletion(
+  commands: DevKit.DK.Commands.Command[]
+) {
+  function tab(indents: number | undefined) {
+    if (indents) return "  ".repeat(indents < 1 ? 1 : indents);
+    else return "  ";
+  }
+  // compdef stuff
+  console.log("#compdef _dkpm dkpm\n");
+  console.log("function _dkpm {"); // begin
+
+  console.log(`${tab(1)}local line\n`);
+  console.log("_arguments -C \\");
+  console.log(`${tab(2)}help, -h, --help => display help`);
+  for (let i = 0; i < commands.length; i++) {
+    const cmd = commands[i];
+    if (cmd.aliases && cmd.aliases.length > 0) {
+      console.log(
+        `${tab(2)}${cmd.commandName}, ${cmd.aliases.concat(", ")} => ${cmd.description} ${
+          i < commands.length - 1 ? "\\" : ""
+        }`
+      );
+    } else
+      console.log(
+        `${tab(2)}${cmd.commandName} => ${cmd.description} ${i < commands.length - 1 ? "\\" : ""}`
+      );
+  }
+  console.log("}"); // end primary function
+}
